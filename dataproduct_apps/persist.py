a5de50b33e29ba6fbbb858a48153c56395db78a2
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-03-07 08:31:02 +0100

    Persist new fields to BQ

--- dataproduct_apps/persist.py
+++ dataproduct_apps/persist.py
@@ -33,12 +33,15 @@ def _persist_records(client, table):
                 row_id = error["index"]
                 row_errors = error["errors"]
                 error_count += 1
-                LOG.fatal("Errors in row %r:\n%s", row_id, "\n".join(_format_bq_error(e) for e in row_errors))
+                LOG.fatal("Errors in row %r:\n%s", row_id, "\n".join(
+                    _format_bq_error(e) for e in row_errors))
             if error_count > 0:
-                LOG.info("Inserted at least %d records in total before error.", row_count)
+                LOG.info(
+                    "Inserted at least %d records in total before error.", row_count)
                 return row_count, error_count
             row_count += len(messages)
-            LOG.info("Inserted %d records from %s", len(messages), topic_partition)
+            LOG.info("Inserted %d records from %s",
+                     len(messages), topic_partition)
     LOG.info("Inserted %d rows", row_count)
     return row_count, error_count
 
@@ -69,26 +72,41 @@ def _format_bq_error(error):
 
 def _init_bq():
     client = bigquery.Client()
-    dataset_ref = DatasetReference(os.getenv("GCP_TEAM_PROJECT_ID"), "dataproduct_apps")
+    dataset_ref = DatasetReference(
+        os.getenv("GCP_TEAM_PROJECT_ID"), "dataproduct_apps")
     table_ref = TableReference(dataset_ref, "dataproduct_apps_v2")
     schema = [
         bigquery.SchemaField(name="collection_time", field_type="DATETIME"),
         bigquery.SchemaField(name="cluster", field_type="STRING"),
         bigquery.SchemaField(name="name", field_type="STRING"),
         bigquery.SchemaField(name="team", field_type="STRING"),
-        bigquery.SchemaField(name="action_url", field_type="STRING", mode="nullable"),
+        bigquery.SchemaField(
+            name="action_url", field_type="STRING", mode="nullable"),
         bigquery.SchemaField(name="namespace", field_type="STRING"),
         bigquery.SchemaField(name="image", field_type="STRING"),
-        bigquery.SchemaField(name="ingresses", field_type="STRING", mode="repeated"),
-        bigquery.SchemaField(name="uses_token_x", field_type="BOOL", mode="nullable"),
-        bigquery.SchemaField(name="inbound_apps", field_type="STRING", mode="repeated"),
-        bigquery.SchemaField(name="outbound_apps", field_type="STRING", mode="repeated"),
-        bigquery.SchemaField(name="outbound_hosts", field_type="STRING", mode="repeated"),
-        bigquery.SchemaField(name="read_topics", field_type="STRING", mode="repeated"),
-        bigquery.SchemaField(name="write_topics", field_type="STRING", mode="repeated"),
-        bigquery.SchemaField(name="databases", field_type="STRING", mode="nullable"),
+        bigquery.SchemaField(
+            name="ingresses", field_type="STRING", mode="repeated"),
+        bigquery.SchemaField(name="uses_token_x",
+                             field_type="BOOL", mode="nullable"),
+        bigquery.SchemaField(name="uses_auto_instrument",
+                             field_type="BOOL", mode="nullable"),
+        bigquery.SchemaField(name="uses_loki_logs",
+                             field_type="BOOL", mode="nullable"),
+        bigquery.SchemaField(name="inbound_apps",
+                             field_type="STRING", mode="repeated"),
+        bigquery.SchemaField(name="outbound_apps",
+                             field_type="STRING", mode="repeated"),
+        bigquery.SchemaField(name="outbound_hosts",
+                             field_type="STRING", mode="repeated"),
+        bigquery.SchemaField(name="read_topics",
+                             field_type="STRING", mode="repeated"),
+        bigquery.SchemaField(name="write_topics",
+                             field_type="STRING", mode="repeated"),
+        bigquery.SchemaField(
+            name="databases", field_type="STRING", mode="nullable"),
         bigquery.SchemaField(name="dbs", field_type="STRING", mode="repeated"),
 
     ]
-    table = client.create_table(bigquery.Table(table_ref, schema=schema), exists_ok=True)
+    table = client.create_table(bigquery.Table(
+        table_ref, schema=schema), exists_ok=True)
     return client, table
